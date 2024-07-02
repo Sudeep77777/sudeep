9ff93f0ba9baee601a6f7d323fd31c58318c670e
Sudeep77777 <sudeepyadav830@gmail.com>
2024-07-02 11:06:16 +0530

    guess1.py

---
+++ guess 1.py
@@ -0,0 +1,36 @@
+import random
+
+def main():
+
+    # initialization
+    high = 0
+    low = 0
+    win = 0
+    number = random.randint(1, 200)
+
+    # input
+    userNum = int(input("Please guess a number between 1 and 200: "))
+
+    # if/else check
+    if userNum > number:
+        message = "Too high, try again."
+        high += 1
+    elif userNum == number:
+        message = "You got it correct! Congratulations!"
+        win += 1
+    else:
+        message = "Too low, try again."
+        low += 1
+    print()
+    print(message)
+
+    # loop
+   # while message != "You got it correct! Congratulations!":
+
+    # display total
+    print()
+    print("Number of times too high: ", high)
+    print("Number of times too low: ", low)
+    print("Total number of guesses: ", (high + low + win))     
+
+main()
